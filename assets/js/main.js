31ccdb4276ca2e598a33bd964f6dafb44ae0cee0
RostiaCotiuba <rostiacotiuba@gmail.com>
2020-12-21 22:34:37 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.dropblock').click(function() {
+      $('.dropdown-advanced-min').slideToggle();
+    });
+  });
+$(function() {
+    $('.droppages').click(function() {
+      $('.dropdown-pages-min').slideToggle();
+    });
+  });
